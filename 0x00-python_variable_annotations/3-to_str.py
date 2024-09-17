41cba3df7b3394798e39ae72a3ca668471a02d67
Cecilia <cecilmutero66@gmail.com>
2024-09-17 14:28:58 +0300

    3. Basic annotations - to string

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -3,6 +3,8 @@
 This module provides a function that converts
 a float to its string representation.
 """
+
+
 def to_str(n: float) -> str:
     """Return the string representation of the float n."""
     return str(n)
